fe29eade9260a961379427f065ebff2c89ccc52e
武琉 本間 <230492@jc-21.jp>
2024-02-01 11:37:21 +0900

    更新

--- Player.cpp
+++ Player.cpp
@@ -1,9 +1,11 @@
 #include "Player.h"
 #include "Engine/Model.h"
 #include "Engine/Input.h"
+#include "Engine/Debug.h"
+#include "Stage.h"
 
 namespace {
-	const float PLAYER_MOVE_SPEED{ 0.1f };
+	const float PLAYER_MOVE_SPEED{ 1.0f }; //スピード
 }
 
 Player::Player(GameObject* parent)
@@ -27,7 +29,7 @@ void Player::Update()
 	//};
 	//int moveDir = Dir::NOME;
 	XMVECTOR vFront = { 0,0,1,0 };
-	XMVECTOR move{ 0, 0, 1, 0 };
+	XMVECTOR move{ 0, 0, 0, 0 };
 
 	if (Input::IsKey(DIK_UP))
 	{
@@ -52,11 +54,28 @@ void Player::Update()
 
 	XMVECTOR pos = XMLoadFloat3(&(transform_.position_));
 	pos = pos + speed_ * move;
-	XMStoreFloat3(&(transform_.position_),pos);
 
-	XMVECTOR vdot = XMVector3Dot(vFront, move);
-	float angle = acos(XMVectorGetX(vdot));
-	transform_.rotate_.y = XMConvertToDegrees(angle);
+	Debug::Log("(X, Y)=");
+	Debug::Log(XMVectorGetX(pos));
+	Debug::Log(",");
+	Debug::Log(XMVectorGetX(pos),true);
+
+	if (!XMVector3Equal(move, XMVectorZero()))
+	{
+		XMStoreFloat3(&(transform_.position_), pos);
+
+		XMVECTOR vdot = XMVector3Dot(vFront, move);
+		assert(XMVectorGetX(vdot) <= 1 && XMVectorGetX(vdot) <= 1);
+
+		float angle = acos(XMVectorGetX(vdot));
+
+		XMVECTOR vCross = XMVector3Cross(vFront, move);
+		if (XMVectorGetY(vCross) < 0)
+		{
+			angle *= -1;
+		}
+		transform_.rotate_.y = XMConvertToDegrees(angle);
+	}
 }
 
 void Player::Draw()
